508628930e8fa0677f31f545502c4adbde67a75e
anhowe <ahowe_ca@yahoo.ca>
2015-12-24 06:23:24 -0800

    explicitly remove the Accept-Encoding before calling mesos

--- common.lua
+++ common.lua
@@ -3,16 +3,24 @@ local common = {}
 local cjson = require "cjson"
 
 function common.mesos_get_state()
+    -- explicitly clear the header.  Both rewrite_by_lua_file and
+    -- more_clear_input_headers happen in phase "rewrite tail"
+    -- and nginx offers no guaranteed ordering within a phase
+    ngx.req.clear_header("Accept-Encoding")
     local res = ngx.location.capture("/mesos/master/state-summary")
     local state = cjson.decode(res.body)
     return state
 end
 
 function common.mesos_dns_get_srv(framework_name)
+    -- explicitly clear the header.  Both rewrite_by_lua_file and
+    -- more_clear_input_headers happen in phase "rewrite tail"
+    -- and nginx offers no guaranteed ordering within a phase
+    ngx.req.clear_header("Accept-Encoding")
     local res = ngx.location.capture("/mesos_dns/v1/services/_" .. framework_name .. "._tcp.marathon.mesos")
     local records = cjson.decode(res.body)
     return records
-end
+ends
 
 function string:split(sep)
     local sep, fields = sep or " ", {}
