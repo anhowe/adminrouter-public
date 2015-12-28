57e31fd0da435050297c06e50d9bb47557faffaf
anhowe <ahowe_ca@yahoo.ca>
2015-12-28 07:46:59 -0800

    fix incorrect variable name

--- slavehostname.lua
+++ slavehostname.lua
@@ -1,7 +1,7 @@
 local common = require "common"
 
 local state = common.mesos_get_state()
-local hostname = ngx.var.slaveid:split(":")[1]
+local hostname = ngx.var.slavehostname:split(":")[1]
 
 for _, slave in ipairs(state["slaves"]) do
     if slave["hostname"] == hostname then
